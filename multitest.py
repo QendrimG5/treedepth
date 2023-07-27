a870e49ec1fdb61526071a145569a988300cf89d
QendrimG5 <38256709+QendrimG5@users.noreply.github.com>
2023-07-27 08:09:51 +0200

    test

--- multitest.py
+++ multitest.py
@@ -2,7 +2,6 @@ import os
 import re
 import subprocess
 import pandas as pd
-import multiprocessing
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
@@ -71,10 +70,8 @@ node_type_selection_probability = {'subtree': 0, 'internal': 10, 'leaf': 10, 'le
 
 new_instance_type = 'heur'
 
-max_threads = multiprocessing.cpu_count()  # get the number of cores
-
-with ThreadPoolExecutor(max_workers=max_threads) as executor:  # using the max threads
-    data = list(executor.map(process_instance, range(1,101)))
+with ThreadPoolExecutor() as executor:
+    data = list(executor.map(process_instance, range(1, 11)))
 
 print("Finished all instances.")
 
